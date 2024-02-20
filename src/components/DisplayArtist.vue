f55d9c34a72a4b75350cc1f15904cbdba99c7840
Bastien Lambert <lambertraverdy@gmail.com>
2024-02-20 11:06:53 +0100

    Requete api fonctionnelle

--- src/components/DisplayArtist.vue
+++ src/components/DisplayArtist.vue
@@ -1,11 +1,29 @@
 <script>
+
+import { ref} from "vue";
+
 export default {
-  name: "DisplayArtist"
+  name: "DisplayArtist",
+  props: {
+    id : String,
+
+  },
+
+  setup(props) {
+    const artist = ref(null);
+    fetch('https://api.deezer.com/artist/'+props.id)
+        .then(response => response.json())
+        .then(data => artist.value = data)
+    return {
+      artist
+    }
+  }
 }
+
 </script>
 
 <template>
-
+  <h1>{{artist}}</h1>
 </template>
 
 <style scoped>
